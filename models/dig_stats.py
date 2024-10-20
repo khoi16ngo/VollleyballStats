f73549a64a7701c2da104694e8721a484040dc82
Khoi <khoi16ngo@gmail.com>
2024-10-20 03:00:52 -0600

    add dig average score to dig stats

--- models/dig_stats.py
+++ models/dig_stats.py
@@ -6,7 +6,7 @@ class DigStats:
         self.great_digs = self._get_great_digs(player_dig_stats)
         self.good_digs = self._get_medium_digs(player_dig_stats)
         self.poor_digs = self._get_poor_digs(player_dig_stats)
-        self.dig_errors = self._get_block_errors(player_dig_stats)
+        self.dig_errors = self._get_dig_errors(player_dig_stats)
         self.total_digs = self._get_total_digs()
 
     def _get_perfect_digs(self, player_dig_stats: list) -> int: 
@@ -21,8 +21,15 @@ class DigStats:
     def _get_poor_digs(self, player_dig_stats: list) -> int: 
         return player_dig_stats[POOR]
 
-    def _get_block_errors(self, player_dig_stats: list) -> int: 
+    def _get_dig_errors(self, player_dig_stats: list) -> int: 
         return player_dig_stats[ERROR]
     
     def _get_total_digs(self) -> int:
         return self.perfect_digs + self.great_digs + self.good_digs + self.poor_digs + self.dig_errors
+
+    def get_dig_average(self) -> float:
+        if self.total_digs == 0:
+            return 0.0
+        
+        total_weighted_digs = (self.perfect_digs * 4) + (self.great_digs * 3) + (self.good_digs * 2) + (self.poor_digs * 1) + (self.dig_errors * 0.5)
+        return total_weighted_digs / self.total_digs 

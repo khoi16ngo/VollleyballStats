b65e09ee64823ac0d02457a648bf3a6f106d93f7
Khoi <khoi16ngo@gmail.com>
2024-10-20 03:01:30 -0600

    add points scored by player to player stat

--- models/player_stat.py
+++ models/player_stat.py
@@ -12,3 +12,9 @@ class PlayerStat:
         self.free_ball_stats = build_player_free_ball_stats(player.number, calculated_player_stats)
         self.serve_receive_stats = build_player_serve_receive_stats(player.number, calculated_player_stats)
         self.serve_stats = build_player_serve_stats(player.number, calculated_player_stats)
+
+    def get_points(self):
+        kills = self.attack_stats.kills
+        aces = self.serve_stats.aces
+        blocks = self.block_stats.perfect_blocks
+        return kills + aces + blocks

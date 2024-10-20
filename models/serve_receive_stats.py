1038992a4dc24b6bf63dbfe5cf21619a47e8c4a4
Khoi <khoi16ngo@gmail.com>
2024-10-20 03:01:44 -0600

    update serve receive stats

--- models/serve_receive_stats.py
+++ models/serve_receive_stats.py
@@ -7,7 +7,7 @@ class ServeReceiveStats:
         self.good_passes = self._get_medium_pass(player_serve_receive_stats)
         self.poor_passes = self._get_poor_pass(player_serve_receive_stats)
         self.pass_errors = self._get_block_errors(player_serve_receive_stats)
-        self.total_passes = self._getTotalPasses()
+        self.total_passes = self._get_total_passes()
 
     def _get_perfect_pass(self, player_serve_receive_stats: list) -> int: 
         return player_serve_receive_stats[PERFECT]
@@ -24,17 +24,24 @@ class ServeReceiveStats:
     def _get_block_errors(self, player_serve_receive_stats: list) -> int: 
         return player_serve_receive_stats[ERROR]
 
-    def _getTotalPasses(self) -> int:
+    def _get_total_passes(self) -> int:
         return self.perfect_passes + self.great_passes + self.good_passes + self.poor_passes + self.pass_errors
 
-    def getInSystemPercentage(self):
+    def get_in_system_percentage(self):
+        if self.total_passes == 0:
+            return 0.0
+        
         return (self.perfect_passes + self.great_passes + self.good_passes) / self.total_passes
 
-    def getOutOfSystemPercentage(self):
+    def get_out_of_system_percentage(self):
+        if self.total_passes == 0:
+            return 0.0
+        
         return self.pass_errors / self.total_passes
     
-    def getPasserAverage(self):
+    def get_pass_average(self):
+        if self.total_passes == 0:
+            return 0.0
+        
         total_weighted_passes = (self.perfect_passes * 4) + (self.great_passes * 3) + (self.good_passes * 2) + (self.poor_passes * 1) + (self.pass_errors * 0.5)
         return total_weighted_passes / self.total_passes 
-
-    
